7c5a47ad3f1fea3d7c077a4231347efcdecdb395
daeunhan <daeunhan0723@gmail.com>
2024-07-25 01:41:56 +0900

    feat: kakao api setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.kakao.maps.open:android:2.6.0")
 }
