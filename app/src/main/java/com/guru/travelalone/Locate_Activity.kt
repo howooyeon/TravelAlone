311bf60bf2c8ed24aa4527a97619dea701bbae61
daeunhan <daeunhan0723@gmail.com>
2024-07-25 11:28:17 +0900

    feat: add activity code

--- app/src/main/java/com/guru/travelalone/Locate_Activity.kt
+++ app/src/main/java/com/guru/travelalone/Locate_Activity.kt
@@ -2,13 +2,21 @@ package com.guru.travelalone
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
 
 class Locate_Activity : AppCompatActivity() {
+
+    lateinit var mapView: MapView
+    lateinit var kakaoMap: KakaoMap
 
     //하단바 ----------
     lateinit var homeButton: ImageButton
@@ -74,5 +82,30 @@ class Locate_Activity : AppCompatActivity() {
             )
             startActivity(intent)
         }
+
+
+        //kakao map api
+        mapView = findViewById(R.id.map_view)
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                // 지도 API가 정상적으로 종료될 때 호출
+                Log.d("KakaoMap", "onMapDestroy: ")
+            }
+
+            override fun onMapError(error: Exception) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출
+                Log.e("KakaoMap", "onMapError: ", error)
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(map: KakaoMap) {
+                // 정상적으로 인증이 완료되었을 때 호출
+                // KakaoMap 객체를 얻어 옵니다.
+                kakaoMap = map
+            }
+        })
+
+//    fun enableEdgeToEdge() {
+//        // Your implementation for enabling edge-to-edge UI
+//    }
     }
 }
