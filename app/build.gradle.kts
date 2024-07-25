57c425d34a70639aa0717faafc55fdf4f5052182
daeunhan <daeunhan0723@gmail.com>
2024-07-25 15:46:09 +0900

    feat: nowlocation setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.kakao.maps.open:android:2.6.0")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
 }
