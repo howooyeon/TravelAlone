0ace54ce717f5aee254a7730e227652deb316ed3
daeunhan <daeunhan0723@gmail.com>
2024-07-29 15:22:28 +0900

    feat: spinner 적용

--- app/src/main/java/com/guru/travelalone/Community_Activity.kt
+++ app/src/main/java/com/guru/travelalone/Community_Activity.kt
@@ -3,20 +3,25 @@ package com.guru.travelalone
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ArrayAdapter
 
 class Community_Activity : AppCompatActivity() {
 
-    //하단바 ----------
+    // 하단바 ----------
     lateinit var homeButton: ImageButton
     lateinit var locateButton: ImageButton
     lateinit var travbotButton: ImageButton
     lateinit var mypageButton: ImageButton
     lateinit var communityButton: ImageButton
-    //하단바 ----------
+    // 하단바 ----------
+
+    // Spinner 추가
+    lateinit var regionSpinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +33,23 @@ class Community_Activity : AppCompatActivity() {
             insets
         }
 
-        //하단바 ----------
+        // 하단바 ----------
         homeButton = findViewById(R.id.homeButton)
         locateButton = findViewById(R.id.locateButton)
         travbotButton = findViewById(R.id.travbotButton)
         mypageButton = findViewById(R.id.mypageButton)
         communityButton = findViewById(R.id.commuButton)
+        // Spinner 초기화
+        regionSpinner = findViewById(R.id.region)
+
+        // Spinner에 어댑터 설정
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.region_list,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        regionSpinner.adapter = adapter
 
         locateButton.setOnClickListener {
             val intent = Intent(
