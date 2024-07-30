1cf2cc3c1e79b890073a14c2bbd0cfa1e5771a41
daeunhan <daeunhan0723@gmail.com>
2024-07-30 18:50:22 +0900

    feat: open gallery

--- app/src/main/java/com/guru/travelalone/Community_Write_Activity.kt
+++ app/src/main/java/com/guru/travelalone/Community_Write_Activity.kt
@@ -1,17 +1,28 @@
 package com.guru.travelalone
 
+import android.Manifest;
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.ArrayAdapter
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
+
 
 class Community_Write_Activity : AppCompatActivity() {
 
@@ -19,6 +30,11 @@ class Community_Write_Activity : AppCompatActivity() {
     private lateinit var regionSpinner: Spinner
     private lateinit var startDateEditText: EditText
     private lateinit var endDateEditText: EditText
+
+    private val PICK_IMAGE_REQUEST: Int = 1
+    private val PERMISSION_REQUEST_CODE: Int = 2
+    private var imageButton: ImageButton? = null
+    private var selectedImageView: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +51,27 @@ class Community_Write_Activity : AppCompatActivity() {
         startDateEditText = findViewById(R.id.start_date)
         endDateEditText = findViewById(R.id.end_date)
 
+        imageButton = findViewById(R.id.imageButton);
+        selectedImageView = findViewById(R.id.selectedImageView);
+
+        imageButton.setOnClickListener(View.OnClickListener { v: View? ->
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSION_REQUEST_CODE
+                )
+            } else {
+                openGallery()
+            }
+        })
+
+
         // Spinner에 어댑터 설정
         val adapter = ArrayAdapter.createFromResource(
             this,
@@ -47,6 +84,35 @@ class Community_Write_Activity : AppCompatActivity() {
         // Set up date pickers
         setUpDatePicker(startDateEditText)
         setUpDatePicker(endDateEditText)
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)}\n      with the appropriate {@link ActivityResultContract} and handling the result in the\n      {@link ActivityResultCallback#onActivityResult(Object) callback}.")
+    protected override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+            val imageUri = data.data
+            selectedImageView!!.setImageURI(imageUri)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun setUpDatePicker(editText: EditText) {
