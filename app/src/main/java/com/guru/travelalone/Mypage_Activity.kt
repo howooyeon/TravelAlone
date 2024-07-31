e260a218dbbd6c17b5b963bc957d920c6e4fae02
hoyeon <aviationlovers@naver.com>
2024-07-31 17:00:15 +0900

    feat : 사용자 정보 불러오기

--- app/src/main/java/com/guru/travelalone/Mypage_Activity.kt
+++ app/src/main/java/com/guru/travelalone/Mypage_Activity.kt
@@ -2,17 +2,23 @@ package com.guru.travelalone
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.guru.travelalone.databinding.ActivityMypageBinding
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.firestore.FirebaseFirestore
+import com.kakao.sdk.user.Constants
+import com.kakao.sdk.user.UserApiClient
 
 class Mypage_Activity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMypageBinding
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +33,8 @@ class Mypage_Activity : AppCompatActivity() {
         }
 
         binding.editButton.setOnClickListener {
-            // 기존에 있는 수정하기 연결
+            val intent = Intent(this, ProEdit_Activity::class.java)
+            startActivity(intent)
         }
 
         binding.locateButton.setOnClickListener {
@@ -70,20 +77,66 @@ class Mypage_Activity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        fetchUserProfile()
         setTabLayout()
+    }
+
+    private fun fetchUserProfile() {
+        try {
+            UserApiClient.instance.me { user, error ->
+                if (error != null) {
+                    Log.e(Constants.TAG, "사용자 정보 요청 실패 $error")
+                    Toast.makeText(this, "사용자 정보 요청 실패", Toast.LENGTH_SHORT).show()
+                } else if (user != null) {
+                    val userId = user.id.toString()
+                    Log.d(Constants.TAG, "사용자 ID: $userId")
+
+                    db.collection("members").document().get()
+                        .addOnSuccessListener { document ->
+                            if (document != null) {
+                                val nickname = document.getString("nickname") ?: ""
+                                val introduction = document.getString("introduction") ?: ""
+                                val profileImageUrl = document.getString("profileImageUrl") ?: ""
+
+                                Log.d(Constants.TAG, "nickname: $nickname")
+                                Log.d(Constants.TAG, "introduction: $introduction")
+                                Log.d(Constants.TAG, "profileImageUrl: $profileImageUrl")
+
+                                binding.nickname.text = nickname
+                                binding.introduction.text = introduction
+
+                                if (profileImageUrl.isNotEmpty()) {
+                                    Glide.with(this)
+                                        .load(profileImageUrl)
+                                        .into(binding.kakaoPro)
+                                }
+                            } else {
+                                Log.d(Constants.TAG, "No such document")
+                                Toast.makeText(this, "No such document", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w(Constants.TAG, "Error getting documents: ", e)
+                            Toast.makeText(this, "Error getting documents", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(Constants.TAG, "사용자 정보 요청 중 예외 발생", e)
+            Toast.makeText(this, "사용자 정보 요청 중 예외 발생", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setTabLayout() {
         // 초기 tab 세팅
-        binding.tabLayoutContainer.setBackgroundResource(R.color.babyblue)
+        binding.tabLayoutContainer.setBackgroundResource(R.color.gray)
 
         binding.tabLayoutContainer.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 when (tab!!.position) {
-                    0 -> binding.tabLayoutContainer.setBackgroundResource(R.color.babyblue)
-                    1 -> binding.tabLayoutContainer.setBackgroundResource(R.color.white)
-                    2 -> binding.tabLayoutContainer.setBackgroundResource(R.color.black)
-                    3 -> binding.tabLayoutContainer.setBackgroundResource(R.color.white)
+                    0 -> binding.tabLayoutContainer.setBackgroundResource(R.color.gray)
+                    1 -> binding.tabLayoutContainer.setBackgroundResource(R.color.gray)
+                    2 -> binding.tabLayoutContainer.setBackgroundResource(R.color.gray)
                 }
             }
 
