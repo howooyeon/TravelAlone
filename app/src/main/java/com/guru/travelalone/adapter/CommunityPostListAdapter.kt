2f7e87f783403e80ac33a65b28a64fd25f28f82f
daeunhan <daeunhan0723@gmail.com>
2024-08-01 18:36:16 +0900

    fix: firebase에서 가져오기

--- app/src/main/java/com/guru/travelalone/adapter/CommunityPostListAdapter.kt
+++ app/src/main/java/com/guru/travelalone/adapter/CommunityPostListAdapter.kt
@@ -4,39 +4,34 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import com.bumptech.glide.Glide
 import com.guru.travelalone.R
 import com.guru.travelalone.item.CommunityPostListItem
 
-class CommunityPostListAdapter(val context: Context, val items : ArrayList<CommunityPostListItem>) : BaseAdapter()
-{
-    override fun getCount(): Int = items.size
+class CommunityPostListAdapter(private val context: Context, private val posts: List<CommunityPostListItem>)
+    : ArrayAdapter<CommunityPostListItem>(context, 0, posts) {
 
-    override fun getItem(position: Int): Any = items[position]
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val post = getItem(position)
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.listview_community_post, parent, false)
 
-    override fun getItemId(position: Int): Long = position.toLong()
+        val imageView = view.findViewById<ImageView>(R.id.image)
+        val profileImageView = view.findViewById<ImageView>(R.id.image_profile)
+        val nameTextView = view.findViewById<TextView>(R.id.name)
+        val titleTextView = view.findViewById<TextView>(R.id.title)
+        val contentTextView = view.findViewById<TextView>(R.id.sub)
+        val dateTextView = view.findViewById<TextView>(R.id.date)
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.listview_community_post, null)
-        val img = view.findViewById<ImageView>(R.id.image)
-        val profile_img = view.findViewById<ImageView>(R.id.image_profile)
-        val title = view.findViewById<TextView>(R.id.title)
-        val name = view.findViewById<TextView>(R.id.name)
-        val sub = view.findViewById<TextView>(R.id.sub)
-        val date = view.findViewById<TextView>(R.id.date)
-        // 북마크 최상위 레이어로 올리기
-        val bookmark = view.findViewById<ImageView>(R.id.bookmark)
-        bookmark.bringToFront()
-
-        val item = items[position]
-        img.setImageDrawable(item.img)
-        profile_img.setImageDrawable(item.profile)
-        title.text = item.title
-        name.text = item.name
-        sub.text = item.sub
-        date.text = item.date
+        Glide.with(context).load(post?.imageUrl).into(imageView)
+        Glide.with(context).load(post?.profileImageUrl).into(profileImageView)
+        nameTextView.text = post?.name
+        titleTextView.text = post?.title
+        contentTextView.text = post?.content
+        dateTextView.text = post?.date
 
         return view
     }
--- app/src/main/java/com/guru/travelalone/item/CommunityPostListItem.kt
+++ app/src/main/java/com/guru/travelalone/item/CommunityPostListItem.kt
@@ -2,5 +2,11 @@ package com.guru.travelalone.item
 
 import android.graphics.drawable.Drawable
 
-data class CommunityPostListItem(var img : Drawable, var profile : Drawable,
-                                 var name : String, var title : String, var sub : String, var date : String)
+data class CommunityPostListItem(
+    val imageUrl: String = "",
+    val profileImageUrl: String = "",
+    val name: String = "",
+    val title: String = "",
+    val content: String = "",
+    val date: String = ""
+)
