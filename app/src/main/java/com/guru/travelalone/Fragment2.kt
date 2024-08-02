40db55b518b237078393d2ae90ae67c92813301a
zen0113 <z_zen@naver.com>
2024-08-02 15:09:37 +0900

    mypage list 수정
    - 카카오 로그인 오류 수정

--- app/src/main/java/com/guru/travelalone/Fragment2.kt
+++ app/src/main/java/com/guru/travelalone/Fragment2.kt
@@ -9,13 +9,18 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
 import com.guru.travelalone.adapter.MypageTripListAdapter
 import com.guru.travelalone.item.MypageTripListItem
+import com.kakao.sdk.user.UserApiClient
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Locale
 
 class Fragment2 : Fragment() {
@@ -37,11 +42,14 @@ class Fragment2 : Fragment() {
         val mypagetripadapter = MypageTripListAdapter(requireContext(), postList)
         mypagetriplistview.adapter = mypagetripadapter
 
-        // Firestore에서 데이터 가져오기
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            val userId = currentUser.uid
-            Log.d("UserID", "Current User ID: $userId")
+
+        fun handleNoValidTripDate(userId: String) {
+            Log.d("UserID", "No valid documents found for user_id: $userId")
+        }
+
+        // tripDate에서 유효한 여행일정 있는지 확인
+        fun handleTripDateDocuments(userId : String) {
+
             db.collection("tripdate")
                 .whereEqualTo("user_id", userId)
                 .get()
@@ -84,7 +92,125 @@ class Fragment2 : Fragment() {
                 .addOnFailureListener { exception ->
                     Toast.makeText(requireContext(), "Error getting documents: $exception", Toast.LENGTH_SHORT).show()
                 }
+
         }
+
+        fun fetchTripDateFromFirebase(userId: String) {
+            Log.d("UserID", "Current User ID: $userId")
+            db.collection("tripdate")
+                .whereEqualTo("user_id", userId)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val documents = task.result
+                        if (documents != null && !documents.isEmpty) {
+                            handleTripDateDocuments(userId)
+
+                        } else {
+                            handleNoValidTripDate(userId)
+                        }
+                    } else {
+                        Log.d("UserID", "Error getting documents: ", task.exception)
+                        handleNoValidTripDate(userId)
+                    }
+                }
+        }
+
+        fun fetchKakaoUserProfileAndTripDate() {
+            UserApiClient.instance.me { user, error ->
+                if (error != null) {
+                    Log.e("Kakao", "사용자 정보 요청 실패", error)
+                    Toast.makeText(requireContext(), "사용자 정보 요청 실패", Toast.LENGTH_SHORT).show()
+                } else if (user != null) {
+                    val kakaoNickname = user.kakaoAccount?.profile?.nickname ?: ""
+                    db.collection("members")
+                        .whereEqualTo("nickname", kakaoNickname)
+                        .get()
+                        .addOnSuccessListener { documents ->
+                            if (documents != null && !documents.isEmpty) {
+                                val email = documents.firstOrNull()?.getString("login_id")
+                                if (email != null) {
+                                    fetchTripDateFromFirebase(email)  // Firebase에서 tripdate 가져오기
+                                } else {
+                                    handleNoValidTripDate("Kakao")  // 이메일이 없는 경우 처리
+                                }
+                            } else {
+                                handleNoValidTripDate("Kakao")  // 사용자 정보가 없는 경우 처리
+                            }
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w("Kakao", "문서 가져오기 실패: ", e)
+                            handleNoValidTripDate("Kakao")
+                        }
+                }
+            }
+        }
+
+        fun fetchTripDate() {
+            val auth = FirebaseAuth.getInstance()
+            val currentUser = auth.currentUser
+
+            if (currentUser != null) {
+                // Firebase 사용자로부터 데이터 가져오기
+                fetchTripDateFromFirebase(currentUser.uid)
+            } else {
+                // Kakao 사용자로부터 데이터 가져오기
+                fetchKakaoUserProfileAndTripDate()
+            }
+        }
+
+
+        // TripDate 불러오면서 사용자 정보 조회
+        fetchTripDate()
+
+        // Firestore에서 데이터 가져오기
+//        val currentUser = auth.currentUser
+//        if (currentUser != null) {
+//            val userId = currentUser.uid
+//            Log.d("UserID", "Current User ID: $userId")
+//            db.collection("tripdate")
+//                .whereEqualTo("user_id", userId)
+//                .get()
+//                .addOnSuccessListener { result ->
+//                    postList.clear() // 데이터가 중복되지 않도록 리스트를 초기화
+//                    for (document in result) {
+//                        val str_title = document.getString("title") ?: ""
+//                        val str_location = document.getString("location") ?: ""
+//                        val long_start_date = document.getLong("start_date") ?: 0L
+//                        val long_end_date = document.getLong("end_date")
+//
+//                        val str_date = if (long_end_date != null && long_end_date.toInt() != 0) {
+//                            "${dateFormat.format(long_start_date)} ~ ${dateFormat.format(long_end_date)}"
+//                        } else {
+//                            "${dateFormat.format(long_start_date)}"
+//                        }
+//
+//                        val drawableRes = when (str_location) {
+//                            "가평/양평" -> R.drawable.img_gapyeong_yangpyeong
+//                            "강릉/속초" -> R.drawable.img_gangneung_sokcho
+//                            "경주" -> R.drawable.img_gyeongju
+//                            "부산" -> R.drawable.img_busan
+//                            "서울" -> R.drawable.img_seoul
+//                            "여수" -> R.drawable.img_yeosu
+//                            "인천" -> R.drawable.img_incheon
+//                            "전주" -> R.drawable.img_jeonju
+//                            "제주" -> R.drawable.img_jeju
+//                            "춘천/홍천" -> R.drawable.img_chuncheon_hongcheon
+//                            "태안" -> R.drawable.img_taean
+//                            "통영/거제/남해" -> R.drawable.img_tongyeong_geoje_namhae
+//                            "포항/안동" -> R.drawable.img_pohang_andong
+//                            else -> R.color.gray // 기본 이미지 설정
+//                        }
+//                        postList.add(MypageTripListItem(ContextCompat.getDrawable(requireContext(), drawableRes)!!, str_title, str_date, str_location))
+//                        Log.d("FirestoreData", "Document data: $document")
+//                    }
+//
+//                    mypagetripadapter.notifyDataSetChanged()
+//                }
+//                .addOnFailureListener { exception ->
+//                    Toast.makeText(requireContext(), "Error getting documents: $exception", Toast.LENGTH_SHORT).show()
+//                }
+//        }
 
         var bt_trip_add : Button = view.findViewById(R.id.bt_add)
         bt_trip_add.setOnClickListener {
