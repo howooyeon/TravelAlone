3d07651adc9c3119eb98c759136de61b5ae9f199
GOOHAESEUNG <amu6675@naver.com>
2024-08-03 20:55:08 +0900

    feat : 로그인 유지, 예ì산 초기화 로직

--- app/src/main/java/com/guru/travelalone/Login_Activity.kt
+++ app/src/main/java/com/guru/travelalone/Login_Activity.kt
@@ -1,23 +1,15 @@
 package com.guru.travelalone
 
-import android.content.ContentValues.TAG
 import android.content.Intent
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
-import android.content.pm.Signature
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.guru.travelalone.databinding.ActivityLoginBinding
 import com.kakao.sdk.auth.AuthApiClient
@@ -25,10 +17,7 @@ import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 class Login_Activity : AppCompatActivity() {
 
@@ -36,6 +25,7 @@ class Login_Activity : AppCompatActivity() {
     private val db = FirebaseFirestore.getInstance()
     private lateinit var auth: FirebaseAuth
 
+    // 카카오 로그인 콜백 함수
     private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error != null) {
             Log.e(TAG, "로그인 실패 $error")
@@ -45,6 +35,7 @@ class Login_Activity : AppCompatActivity() {
         }
     }
 
+    // 버튼 클릭 리스너
     private val onClickListener = View.OnClickListener { v ->
         when (v?.id) {
             binding.btnLogin.id -> {
@@ -78,15 +69,23 @@ class Login_Activity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
-        // Load the Kakao API Key from BuildConfig
+        // Firebase Auth 초기화
+        auth = FirebaseAuth.getInstance()
+
+        // 카카오 API 키 로드
         val kakaoApiKey = BuildConfig.KAKAO_API_KEY
 
-        // Initialize Kakao SDK
+        // 카카오 SDK 초기화
         KakaoSdk.init(this, kakaoApiKey)
 
-        // Log the key hash
-        // Log.d(TAG, "Key Hash: ${getKeyHash()}")
+        // Firebase에 로그인된 사용자가 있는지 확인
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            // 사용자가 로그인된 경우 Home_Activity로 이동
+            moveHomePage(currentUser)
+        }
 
+        // 카카오에 로그인된 사용자가 있는지 확인
         if (AuthApiClient.instance.hasToken()) {
             UserApiClient.instance.accessTokenInfo { _, error ->
                 if (error == null) {
@@ -97,23 +96,13 @@ class Login_Activity : AppCompatActivity() {
 
         binding.btnLogin.setOnClickListener(onClickListener)
 
-        auth = Firebase.auth
-
         binding.emailLoginBtn.setOnClickListener {
             email_login()
         }
-
-
-//        binding.release.setOnClickListener{
-//            val intent = Intent(this@Login_Activity, Release_Activity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }
-
     }
 
+    // 사용자 프로필 확인
     private fun checkUserProfile() {
-
         UserApiClient.instance.me { user, error ->
             val targetNickname = user?.kakaoAccount?.profile?.nickname
 
@@ -123,91 +112,73 @@ class Login_Activity : AppCompatActivity() {
                 val intent = Intent(this, Login_Activity::class.java)
                 startActivity(intent)
                 finish()
-//            } else if (user != null && user.kakaoAccount?.profile?.nickname == null) {
-//                // 사용자 정보가 있을 때 MainActivity로 이동
-//                val intent = Intent(this, MainActivity::class.java)
-//                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//                startActivity(intent)
-//                finish()
-//            } else {
-//                // 사용자 정보가 없을 때 ProEdit_Activity로 이동
-//
-//            }
             } else {
                 db.collection("members")
                     .whereEqualTo("nickname", targetNickname)
                     .get()
                     .addOnSuccessListener { result ->
                         if (result.isEmpty) {
-                            Log.d("NoMatch", "No documents found with nickname: $targetNickname")
+                            Log.d("NoMatch", "닉네임이 일치하는 문서가 없음: $targetNickname")
                             val intent = Intent(this, ProEdit_Activity::class.java)
-                            intent.flags =
-                                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                             startActivity(intent)
                             finish()
                         } else {
                             for (document in result) {
                                 Log.d("DocumentID", document.id)
                                 val intent = Intent(this, Home_Activity::class.java)
-                                intent.flags =
-                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                                 startActivity(intent)
                                 finish()
                             }
                         }
                     }
                     .addOnFailureListener { exception ->
-                        Log.w("Error", "Error getting documents: ", exception)
+                        Log.w("Error", "문서 가져오기 오류: ", exception)
                     }
             }
-
         }
     }
 
+    // 이메일 로그인
     fun email_login() {
-        if (binding.emailEdt.text.toString().isNullOrEmpty() || binding.pwEdt.text.toString()
-                .isNullOrEmpty()
-        ) {
+        if (binding.emailEdt.text.toString().isNullOrEmpty() || binding.pwEdt.text.toString().isNullOrEmpty()) {
             Toast.makeText(this, "이메일 혹은 비밀 번호를 입력해주세요", Toast.LENGTH_SHORT).show()
         } else {
             signinAndssignup()
         }
     }
 
+    // 이메일로 회원가입 및 로그인
     fun signinAndssignup() {
-        auth.createUserWithEmailAndPassword(
-            binding.emailEdt.text.toString(),
-            binding.pwEdt.text.toString()
-        )
+        auth.createUserWithEmailAndPassword(binding.emailEdt.text.toString(), binding.pwEdt.text.toString())
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    //이메일로 성공적으로 계정을 만들었을 경우
+                    // 이메일로 성공적으로 계정을 만들었을 경우
                     moveProEditPage(task.result?.user)
                 } else if (task.exception?.message.isNullOrEmpty()) {
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
                 } else {
-                    //이메일의 계정이 이미 존재하는 경우
+                    // 이메일의 계정이 이미 존재하는 경우
                     sigininEmail()
                 }
             }
     }
 
+    // 이메일로 로그인
     fun sigininEmail() {
-        auth.signInWithEmailAndPassword(
-            binding.emailEdt.text.toString(),
-            binding.pwEdt.text.toString()
-        )
+        auth.signInWithEmailAndPassword(binding.emailEdt.text.toString(), binding.pwEdt.text.toString())
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    //로그인 성공
+                    // 로그인 성공
                     moveHomePage(task.result?.user)
                 } else {
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
                 }
-
             }
     }
 
+    // Home_Activity로 이동
     fun moveHomePage(user: FirebaseUser?) {
         if (user != null) {
             startActivity(Intent(this, Home_Activity::class.java))
@@ -215,6 +186,7 @@ class Login_Activity : AppCompatActivity() {
         }
     }
 
+    // ProEdit_Activity로 이동
     fun moveProEditPage(user: FirebaseUser?) {
         if (user != null) {
             startActivity(Intent(this, NormalPro_Activity::class.java))
@@ -222,5 +194,7 @@ class Login_Activity : AppCompatActivity() {
         }
     }
 
-
+    companion object {
+        private const val TAG = "Login_Activity"
+    }
 }
