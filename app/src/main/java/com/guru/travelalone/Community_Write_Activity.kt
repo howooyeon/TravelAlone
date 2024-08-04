f0335ea9f974de4cd321c086634b34f0573edcc6
daeunhan <daeunhan0723@gmail.com>
2024-08-04 16:15:33 +0900

    fix: 첨부했던 이미지 불러오기

--- app/src/main/java/com/guru/travelalone/Community_Write_Activity.kt
+++ app/src/main/java/com/guru/travelalone/Community_Write_Activity.kt
@@ -21,6 +21,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import coil.load
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
@@ -183,6 +184,7 @@ class Community_Write_Activity : AppCompatActivity() {
                     }
 
                     if (selectedImageUri != null) {
+                        selectedImageView.load(selectedImageUri)
                         selectedImageView.visibility = View.VISIBLE
                         cardView.visibility = View.VISIBLE
                     } else {
